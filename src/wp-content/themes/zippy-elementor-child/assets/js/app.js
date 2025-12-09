fca3765bb8935dca0282076b3a46dc6531be0b70
Lucass1611 <chuong.nguyenquoc@floatingcube.com>
2025-12-09 10:34:13 +0700

    init code

--- src/wp-content/themes/zippy-elementor-child/assets/js/app.js
+++ src/wp-content/themes/zippy-elementor-child/assets/js/app.js
@@ -1,3 +1,17 @@
 $(document).ready(function ($) {
-  
+  // Init variables
+  let active_tab = $("#hp-product-tabs-block .tab-nav-list").find(".active");
+  let selected_tab = $("#hp-product-tabs-block .tab-nav-list").find(".tab-nav");
+
+  // Action
+  selected_tab.on("click", function () {
+    updateUISelectedTab();
+  });
+  // Function
+  function updateUISelectedTab() {
+    active_tab.css({
+      background: "#0B2743",
+      color: "#ffffffff",
+    });
+  }
 });
